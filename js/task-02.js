40e9fedcd64b56a6be12014d2e56ea91904e4321
Joanna <joanna.garstka92@gmail.com>
2023-02-24 16:04:33 +0100

    Zadanie 2 zmiana

--- js/task-02.js
+++ js/task-02.js
@@ -8,7 +8,6 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector('#ingredients');
-ingredientsList.classList.add('item');
 
 const title = document.createElement('h2');
 title.textContent = 'INGREDIENTS LIST';
@@ -18,4 +17,5 @@ ingredients.forEach(ingredient => {
   const element = document.createElement('li');
   element.textContent = ingredient;
   ingredientsList.append(element);
+  ingredientsList.classList.add('item');
 });
