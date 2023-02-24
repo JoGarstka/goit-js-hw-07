27a045e5ce4508e8728d007ac02eaf82bbc408ef
Joanna <joanna.garstka92@gmail.com>
2023-02-24 15:44:14 +0100

    Zadanie 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.classList.add('item');
+
+const title = document.createElement('h2');
+title.textContent = 'INGREDIENTS LIST';
+ingredientsList.before(title);
+
+ingredients.forEach(ingredient => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  ingredientsList.append(element);
+});
