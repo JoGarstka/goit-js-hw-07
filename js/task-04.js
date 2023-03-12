9d59637b4b1ccd99c80432ac7bf2e8a163317da8
Joanna <joanna.garstka92@gmail.com>
2023-03-12 13:54:20 +0100

    Zadanie 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxes = document.getElementById('boxes');
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.height = `${size}px`;
+    box.style.width = `${size}px`;
+    boxes.appendChild(box);
+    size += 10;
+  }
+}
+
+const create = document.querySelector('button[data-create]');
+create.addEventListener('click', () => {
+  let amount = document.querySelector('input').value;
+  createBoxes(amount);
+});
+
+function destroyBoxes() {
+  const boxes = document.getElementById('boxes');
+  boxes.innerHTML = '';
+}
+
+const destroy = document.querySelector('button[data-destroy]');
+destroy.addEventListener('click', () => {
+  destroyBoxes();
+});
